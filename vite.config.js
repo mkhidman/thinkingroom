456639429afb324323abb35382d902ae0b8dd85b
Khidir Abdurrohman <m.khidman@gmail.com>
2026-07-22 16:19:32 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -14,10 +14,12 @@ export default defineConfig({
                 theme_color: '#005BAC',
                 background_color: '#F4F7FB',
                 display: 'standalone',
+                id: '/',
+                scope: '/',
                 start_url: '/',
                 icons: [
-                    { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-                    { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+                    { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+                    { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
                 ]
             }
         })
